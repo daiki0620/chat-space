b86ff23e940b8d96589df54a461e1c45f2204174
daiki0620 <sasakidaiki@sasakidaikinoMacBook-puro.local>
2018-05-15 16:57:25 +0900

    グループ編集機能の実装

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,5 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
   def index
   end
   def new
@@ -15,7 +16,7 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:id])
+
   end
 
   def update
