d1c325055b44688ea84c264fc7b33e988d59234c
daiki0620 <sasakidaiki@sasakidaikinoMacBook-puro.local>
2018-05-28 13:30:25 +0900

    インクリメンタルサーチの実装

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -13,23 +13,27 @@ $(function(){
                     buildhtml(user);
                   }
                  function buildhtml(user){
+                  addhtml();
                   $('.chat-group-user__btn--ad').on('click',function(){
-                    $('.chat-group-user__name').empty();
-                    $(this).empty();
                         var html= `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
                             <input name='group[user_ids][]' type='hidden' value=${user.user_id}>
-                            <p class='chat-group-user__name'>${user.name}</p>
+                            <p class='chat-group-user__names'>${user.name}</p>
                             <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'><span class="destroy-btn">削除</span></a>
                         </div>`
                   chat_user_name.append(html);
                   destroyhtml();
                  });
                 }
+                function addhtml(){
+                  $('.chat-group-user__btn--ad').on('click',function(){
+                  $(this).empty();
+                  $('.chat-group-user__name').empty();
+                })
+               };
                 function destroyhtml(){
                   $('.destroy-btn').on('click',function(){
                   $(this).empty();
                   $('.chat-group-user__name').empty();
-                  console.log('ok')
                  });
                 }
   $('.chat-group-form__input').on("keyup", function(){
